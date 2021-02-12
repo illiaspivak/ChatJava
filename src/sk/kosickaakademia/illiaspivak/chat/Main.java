46a9265b5d52dc760fa8991d70505aca88b343c2
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-02-12 13:05:09 +0100

    The MD5 message-digest algorithm

--- src/sk/kosickaakademia/illiaspivak/chat/Main.java
+++ src/sk/kosickaakademia/illiaspivak/chat/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sk.kosickaakademia.illiaspivak.chat.util.Util;
 
 public class Main extends Application {
 
@@ -18,8 +19,11 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        Database database = new Database();
-        database.TestConnection();
-        launch(args);
+//        Database database = new Database();
+//        database.TestConnection();
+//        launch(args);
+        Util util = new Util();
+        String example = "example";
+        System.out.println(util.getMD5(example));
     }
 }
